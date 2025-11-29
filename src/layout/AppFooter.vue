3ec196dc38f0edf1083b8614c92cdce06c3bf3eb
Hamilton Patiño Solano <hamiltonpatinosolano@gmail.com>
2025-11-29 10:26:59 -0500

    Footer visible ne todo momento

--- src/layout/AppFooter.vue
+++ src/layout/AppFooter.vue
@@ -2,7 +2,7 @@
 
 <template>
     <div class="layout-footer">
-        SAKAI by
-        <a href="https://primevue.org" target="_blank" rel="noopener noreferrer" class="text-primary font-bold hover:underline">PrimeVue</a>
+        Dayvo By
+        <a href="https://makinelab.com" target="_blank" rel="noopener noreferrer" class="text-primary font-bold hover:underline ml-1">Makinelab</a>
     </div>
 </template>
